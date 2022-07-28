e3a3aa269334e87d8561c5ea891b3acd2e23b2ba
Anish Rawat <109730585+Anish-Rawat@users.noreply.github.com>
2022-07-29 00:35:00 +0530

    Create inverted_half_pyramid.java

---
+++ pattern/inverted_half_pyramid.java
@@ -0,0 +1,19 @@
+/*
+	Print inverted half pyramid 
+	*****
+	****
+	***
+	**
+	*
+*/
+
+class invertedhalfpyramid{
+	public static void main(String args[]){
+		for(int i=0;i<5;i++){
+			for(int j=5;j>i;j--){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
